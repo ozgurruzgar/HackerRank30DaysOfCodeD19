679a902ac1275491b68739f3971004b1d17ce6d0
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-15 11:45:09 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCodeD19
+{
+    class Program
+    {
+        public interface AdvancedArithmetic
+        {
+            int divisorSum(int n);
+        }
+
+        public class Calculator : AdvancedArithmetic
+        {
+            public int divisorSum(int n)
+            {
+                var sum = 0;
+
+                for (var i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                return sum;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
+}
